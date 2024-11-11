0a729b34e627aa4ff92e0d9979a492b9ef8991d6
vishalpwaman <vishalpwaman1997@gmail.com>
2024-11-11 15:12:43 +0530

    Added : CreateShoppingList, AddItem & RemoveItem Method

--- SignalR.Web.Application/ShoppingListHub.cs
+++ SignalR.Web.Application/ShoppingListHub.cs
@@ -1,9 +1,39 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 
 namespace SignalR.Web.Application
 {
     public class ShoppingListHub : Hub
     {
+
+        private static readonly ConcurrentDictionary<string, List<string>> _groupIdShoppingLists = new();
+
+        public async Task CreateShoppingList()
+        {
+            string shoppingListId  = Guid.NewGuid().ToString();
+            _groupIdShoppingLists.TryAdd(shoppingListId, new List<string>());
+            await Groups.AddToGroupAsync(Context.ConnectionId, shoppingListId);
+            await Clients.Caller.SendAsync("ShoppingListCreated", shoppingListId);
+        }
+
+        public async Task AddItem(string shoppingListId, string itemName)
+        {
+            if(_groupIdShoppingLists.TryGetValue(shoppingListId, out var list))
+            {
+                list.Add(itemName);
+                await Clients.Group(shoppingListId).SendAsync("ReceiveShoppingList", list);
+            }
+        }
+
+        public async Task RemoveItem(string shoppingListId, string itemName)
+        {
+            if (_groupIdShoppingLists.TryGetValue(shoppingListId, out var list))
+            {
+                list.Remove(itemName);
+                await Clients.Group(shoppingListId).SendAsync("ReceiveShoppingList", list);
+            }
+        }
+
         //By Default Method
         /*public override async Task OnConnectedAsync()
         {
@@ -18,5 +48,7 @@ namespace SignalR.Web.Application
         {
             return base.OnDisconnectedAsync(exception);
         }*/
+
+
     }
 }
